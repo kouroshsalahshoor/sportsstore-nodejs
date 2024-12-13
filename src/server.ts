1022b87c6a76c58b8c6e54799ecc99fb2791ebe8
kourosh <kourosh23@hotmail.com>
2024-12-13 18:12:40 +0100

    routes

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,16 @@ import { createServer } from "http";
 import express, { Express } from "express";
 import helmet from "helmet";
 import { getConfig } from "./config";
+import { createRoutes } from "./routes";
 
 const port = getConfig("http:port", 5000);
 const expressApp: Express = express();
 
 expressApp.use(helmet());
 expressApp.use(express.json());
-expressApp.use(express.urlencoded({ extended: true }))
-expressApp.get("/", (req, resp) => {
-    resp.send("Hello, SportsStore");
-})
+expressApp.use(express.urlencoded({ extended: true }));
+
+createRoutes(expressApp);
 
 const server = createServer(expressApp);
 server.listen(port,
