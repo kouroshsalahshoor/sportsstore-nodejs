fe082e586f7d8f5f248da7000576755b6b5264ac
kourosh <kourosh23@hotmail.com>
2025-02-01 11:09:54 +0100

    ch1 tools

---
+++ dist/Demos/ch1/tools/message.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+for command history:
+    > h
+
+   1 pwd
+   2 cd C:\Users\kouro\Desktop\sportsstore-nodejs\src\Demos\ch1\tools
+   3 node hello.js
+   4 npm init -y
+   5 npm install bootstrap@5.3.0
+   6 npm install --save-dev typescript@5.2.2 tsc-watch@6.0.4
+   7 npm install --save-dev nodemon@2.0.20
+   8 tsc message.ts
+   9 node .\message.js
+  10 npx tsc message.ts
+  11 npx --package=typescript tsc message.ts
+  12 npm start
+  13 npm run go
+*/
+function writeMessage(msg) {
+    console.log(`Message: ${msg}`);
+}
+writeMessage("This is the new message");
